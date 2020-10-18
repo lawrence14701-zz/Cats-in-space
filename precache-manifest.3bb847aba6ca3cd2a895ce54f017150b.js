c6f8fc0584735dafa37498943c239dfbcf7c97b9
LAWRENCE NICASTRO <lawrencenicastro@LAWRENCEs-MacBook-Pro.local>
2020-10-17 23:42:07 -0400

    Updates

---
+++ precache-manifest.3bb847aba6ca3cd2a895ce54f017150b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "729170ee2b1d0b88d5da7cb2e64a2a50",
+    "url": "/cats/index.html"
+  },
+  {
+    "revision": "0d4511790726a48692f5",
+    "url": "/cats/static/css/main.48e2f1f3.chunk.css"
+  },
+  {
+    "revision": "d10831ad4a66af4075d2",
+    "url": "/cats/static/js/2.2fdca914.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/cats/static/js/2.2fdca914.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d4511790726a48692f5",
+    "url": "/cats/static/js/main.1eebef28.chunk.js"
+  },
+  {
+    "revision": "61672db19e222538f9da",
+    "url": "/cats/static/js/runtime-main.66fb319a.js"
+  },
+  {
+    "revision": "3573115a2acae4f372df408ba5c60a25",
+    "url": "/cats/static/media/space.3573115a.jpg"
+  }
+]);
